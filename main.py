1e6eaa1e653e412a320b13a46aeeca701e00eca3
sosaaa123 <lautasosita097@gmail.com>
2025-11-11 20:49:32 -0300

    eee

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ arg = pytz.timezone('America/Argentina/Buenos_Aires')
 
 cur = conexion.cursor()
 
-origenes = ["http://localhost:5173", "https://front-votos.vercel.app"]
+origenes = ["http://localhost:5173", "https://front-votos.vercel.app", "https://dashboard-votos-skea.vercel.app/"]
 
 app = FastAPI()
 
